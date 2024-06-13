d0c1bdc44900baed283c1ff748e79c237573037b
ChenFryd <chenfrydman@gmail.com>
2024-06-13 12:07:31 +0300

    uploaded App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <template>
   <div id="app">
-
-    <div id="nav">
-      <router-link :to="{ name: 'main' }">Recipes</router-link>|
-      <router-link :to="{ name: 'search' }">Search</router-link>|
-      <router-link :to="{ name: 'about'}"> About</router-link>
-      {{ !$root.store.username }}
-      <span v-if="!$root.store.username">
-        Guest:
-        <h2>hello guest</h2>
-        <router-link :to="{ name: 'register' }">Register</router-link>|
-        <router-link :to="{ name: 'loginpage' }">Login</router-link>|
-      </span>
-      <span v-else>
-         <b-button @click="toggleModal">Add New Recipe</b-button>
-        <b-dropdown id="dropdown-left" text="Personal Info" variant="primary" class="m-2">
-        <b-dropdown-item href="#"><router-link :to="{ name: 'favorites' }">Favorites</router-link></b-dropdown-item>
-        <b-dropdown-item href="#"><router-link :to="{ name: 'myrecipes' }">My Recipes</router-link></b-dropdown-item>
-        <b-dropdown-item href="#"><router-link :to="{ name: 'familyrecipes' }">Family Recipes</router-link></b-dropdown-item>
-        </b-dropdown>
-        <h1 class="user-greeting">Hello, {{ $root.store.username }}</h1>|
-        {{ $root.store.username }}: <button @click="Logout">Logout</button>|
-      </span>
-    </div>
+    <Navbar />
     <router-view />
     <main-page></main-page>
   </div>
@@ -31,26 +9,21 @@
 <script>
 
 import MainPage from './pages/MainPage.vue'; 
+import Navbar from './components/Navbar.vue';
+import OldNavbar from './components/OldNavbar.vue';
+
 export default {
   name: "App",
   components:{
-    MainPage
+    MainPage,
+    OldNavbar,
+    Navbar
   },
-  
-  methods: {
-    Logout() {
-      this.$root.store.logout();
-      this.$root.toast("Logout", "User logged out successfully", "success");
+}
 
-      this.$router.push("/").catch(() => {
-        this.$forceUpdate();
-      });
-    }
-  }
-};
 </script>
 
-<style lang="scss">
+<style>
 @import "@/scss/form-style.scss";
 
 #app {
@@ -59,18 +32,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   min-height: 100vh;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
